e7d9f89655ecac28de486579e413fb270b97ae94
Ephraim A. <126341856+tekika1@users.noreply.github.com>
2025-09-09 17:29:13 +0100

    adding the logic for detection and boundary construction around the object

--- gun detection.py
+++ gun detection.py
@@ -9,6 +9,29 @@ camera = cv2.VideoCapture(0)
 firstFrame = None
 gun_exist = None
 
+while True:
+
+    ret, frame = camera.read()
+
+    frame = imutils.resize(frame,width=500)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    gun = gun_cascade.detectMultiScale(gray,
+                                       1.3,5,
+                                       minSize=(100,100))
+    
+    if len(gun) > 0:
+        gun_exist = True
+
+
+    for(x,y,w,h) in gun:
+        frame = cv2.rectangle(frame,
+                              (x,y)
+                              (x+w,y+h)
+                              (255,0,0),2)
+
+
+
 
 
 
