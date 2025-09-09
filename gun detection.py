45e98fa4df840ac2bf9f749d798c82be9b07f238
Ephraim A. <126341856+tekika1@users.noreply.github.com>
2025-09-09 14:32:41 +0100

    added cascade xml and initialised frame and gun variables

--- gun detection.py
+++ gun detection.py
@@ -1,2 +1,14 @@
 import numpy as np
 import cv2 
+import imutils
+import datetime
+
+gun_cascade = cv2.CascadeClassifier('cascade.xml')
+camera = cv2.VideoCapture(0)
+
+firstFrame = None
+gun_exist = None
+
+
+
+
